23d13c75d8462d5b7dd6d46dd0d14c8788048c31
jessicaherford <jessicaherford@yahoo.com>
2015-11-02 09:14:38 -0700

    Updates

---
+++ script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+   // $('.divOne').hide();
+   // $('.divOne').show("slide"), ({ direction: "right" }, 25000);
+  
+
+    //$('.divTwo').hide();
+    //$('.divTwo').show("slide"), ({ direction: "left"}, 25000);
+
+   // $('.divThree').hide();
+   // $('.divThree').show("slide"), ({ direction: "right"}, 25000);
+
+});
