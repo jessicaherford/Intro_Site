105638aedfb09220c72346acf1a782daa7f778d0
jessicaherford <jessicaherford@yahoo.com>
2015-11-06 15:18:15 -0700

    Intro_Site Merge with prework repository

--- script.js
+++ script.js
@@ -8,12 +8,25 @@ $( document ).ready(function() {
      $(this).css('border-style', 'solid');
    })
 
+   $('button').mouseenter(function() {
+     $(this).css('background-color', '#722364');
+     $(this).css('color', 'white');
+   });
+
+   $('button').mouseleave(function() {
+     $(this).css('background-color', 'white');
+     $(this).css('color', '#722364');
+     $(this).css('font-weight', 'bold');
+     $(this).css('font-size', '15px');
+   });
+
    $('button').click(function(){
      $('html').css('background-color', 'black');
      $('.section').css('border-color', 'white');
      $('h1').css('color', 'white');
      $('p').css('color', 'white');
    })
+
 
 
 });
